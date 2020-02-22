30736f8f05e7af8df422e8c55a044a710cc2b549
Ryan Huse <ryan.huse@alaskaair.com>
2020-02-22 11:12:37 -0800

    It can run now

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// API calls
 app.get("/api/hello", (req, res) => {
   res.send({ express: "Hello From Express" });
 });
@@ -17,5 +20,15 @@ app.post("/api/world", (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`
   );
 });
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
